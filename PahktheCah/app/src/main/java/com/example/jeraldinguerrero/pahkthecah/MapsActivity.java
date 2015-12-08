f377ce44ee667ca6410df9d2dcc567fd15c1a1eb
Jeraldin Guerrero <jguerero@bu.edu>
2015-12-08 02:03:55 -0500

    Google Maps API
    Attempt: Julian

--- PahktheCah/app/src/main/java/com/example/jeraldinguerrero/pahkthecah/MapsActivity.java
+++ PahktheCah/app/src/main/java/com/example/jeraldinguerrero/pahkthecah/MapsActivity.java
@@ -1,6 +1,185 @@
 package com.example.jeraldinguerrero.pahkthecah;
 
-import android.content.Intent;
+
+import android.location.Location;
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.widget.Toast;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MapsActivity extends FragmentActivity implements
+        GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, GoogleMap.OnMapClickListener, GoogleMap.OnMapLongClickListener
+{
+    private GoogleMap mMap; // Might be null if Google Play services APK is not available.
+    //Google maps location (used for button)
+    private Location mLastLocation;
+
+    // Google client to interact with Google API
+    private GoogleApiClient mGoogleApiClient;
+
+    //When the app is open
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        setUpMapIfNeeded();
+        //Making sure Google maps is gucci
+        if (checkPlayServices())
+        {
+            // Building the GoogleApi client
+            buildGoogleApiClient();
+        }
+        //Declaring map dependencies
+        displayLocation();
+        onMapReady(mMap);
+    }
+
+    //Creates the map
+    private void setUpMap()
+    {
+        //Live locations
+        mMap.setMyLocationEnabled(true);
+        //Remove buildings
+        mMap.setBuildingsEnabled(false);
+        //Turn off basic menu
+        mMap.getUiSettings().setMapToolbarEnabled(false);
+        //Set up map click listeners
+        mMap.setOnMapClickListener(this);
+        mMap.setOnMapLongClickListener(this);
+        // Zoom in the Google Map
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(42.3496, -71.0997), 14.0f));
+        // mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(39.8282, -98.5795), 3.0f));
+    }
+    //Set up the map the first time
+    private void setUpMapIfNeeded()
+    {
+        // Do a null check to confirm that we have not already instantiated the map.
+        if (mMap == null) {
+            // Try to obtain the map from the SupportMapFragment.
+            mMap = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map))
+                    .getMap();
+            // Check if we were successful in obtaining the map.
+            if (mMap != null)
+            {
+                setUpMap();
+            }
+        }
+    }
+    //Passes in Map to do some intialization nothing in this case
+    public void onMapReady(GoogleMap map)
+    {
+        map.addMarker(new MarkerOptions()
+                .position(new LatLng(42.349220, -71.106121))
+                .title("Photonics Center"));
+    }
+    //Connects app to Google API
+    @Override
+    protected void onStart()
+    {
+        super.onStart();
+        if (mGoogleApiClient != null)
+        {
+            mGoogleApiClient.connect();
+        }
+        displayLocation();
+    }
+    //Upon returning to app
+    @Override
+    protected void onResume()
+    {
+        super.onResume();
+        setUpMapIfNeeded();
+    }
+    //Detecting a single map click
+    public void onMapClick(LatLng point)
+    {
+
+    }
+    //Detecing a long click (pressing a while on the map)
+    public void onMapLongClick(LatLng point)
+    {
+        ;
+    }
+    //When connected to API
+    @Override
+    public void onConnected(Bundle bundle)
+    {
+        displayLocation();
+    }
+    //When disconnected, try to recon
+    @Override
+    public void onConnectionSuspended(int i)
+    {
+        mGoogleApiClient.connect();
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult)
+    {
+
+    }
+    //Connecting to actual client
+    protected synchronized void buildGoogleApiClient()
+    {
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API).build();
+    }
+    //Checking if we are connected
+    private boolean checkPlayServices()
+    {
+        int resultCode = GooglePlayServicesUtil
+                .isGooglePlayServicesAvailable(this);
+        if (resultCode != ConnectionResult.SUCCESS)
+        {
+            if (GooglePlayServicesUtil.isUserRecoverableError(resultCode))
+            {
+
+            }
+            else
+            {
+                Toast.makeText(getApplicationContext(),
+                        "This device is not supported.", Toast.LENGTH_LONG)
+                        .show();
+                finish();
+            }
+            return false;
+        }
+        else
+        {
+            return true;
+        }
+    }
+    //Update the live location dot
+    private void displayLocation()
+    {
+        mLastLocation = LocationServices.FusedLocationApi
+                .getLastLocation(mGoogleApiClient);
+
+        if (mLastLocation != null)
+        {
+            double latitude = mLastLocation.getLatitude();
+            double longitude = mLastLocation.getLongitude();
+            // mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(latitude, longitude), 14.0f));
+        }
+        else
+        {
+
+        }
+    }
+}
+/*import android.content.Intent;
 import android.net.Uri;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
@@ -14,6 +193,7 @@ import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
+    private GoogleMap map;
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
@@ -34,8 +214,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
      * If Google Play services is not installed on the device, the user will be prompted to install
      * it inside the SupportMapFragment. This method will only be triggered once the user has
      * installed Google Play services and returned to the app.
-     */
-    @Override
+
+
     public void onMapReady(GoogleMap googleMap) {
 
         Throwable throwable = new Throwable();
@@ -425,3 +605,4 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         }
     }
 }
+ */
