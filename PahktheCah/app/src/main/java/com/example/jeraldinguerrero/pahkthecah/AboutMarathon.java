a4aaa69240e939972b84d50ee9c6a64acf7661a0
mgarcia7 <mgarcia7@bu.edu>
2015-12-11 15:19:08 -0500

    Cleaned up repository/deleted unnecessary files

--- PahktheCah/app/src/main/java/com/example/jeraldinguerrero/pahkthecah/AboutMarathon.java
+++ PahktheCah/app/src/main/java/com/example/jeraldinguerrero/pahkthecah/AboutMarathon.java
@@ -9,58 +9,44 @@ import android.view.View.OnClickListener;
 import android.widget.Button;
 
 
-/*
-NOTES:
--figure out how to come back from google and where it comes back to
-	-does it go to on pause in main or if we have to make onpause in maps activity . java
-	-make a new intent and go to page "did you get there" and create that page
-	-once you're there, yes and no should send them to options
-		-if they press yes, press image to black and white (figure out how to do black and white)
-	-yes take a pic, do camera stuff, what happens when camera comes back
-		-back to options page
--load xml files in the project
--1 classes: did you get there?
-*/
-
 /**
  * Created by jeraldinguerrero on 12/5/15.
  */
 
-//the aboutMarathon is the class 
-//extends Activity is similar to namespace from my understanding?
+//the aboutMarathon is the class
 public class AboutMarathon extends Activity implements OnClickListener{
 
     //this is Button (which is a type) && google is the name of the button
     private Button google;
 
-    //override is similar to overload in c++ because we have a onCreate function in the main activity
     //onCreate means what happens when this activity starts 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
-        //i dont really get this but it's some reserved word called super that does something
-        //julian said it was important 
         super.onCreate(savedInstanceState);
 
         //calls the appropriate xml file to mame it look like that 
         setContentView(R.layout.activity_about_marathon);
+
         //in that xml file is a button called googleButton 
         //we want to get the id of that button and set it equal to our google button 
         google = (Button) findViewById(R.id.googleButton);
+
         //sets onclicklistener
         google.setOnClickListener(this);
     }
 
-    //same deal with override
     //this listens for any button pressed 
     //in our case there's just one on this page so no need for if or switch statements
     //takes in a View  (a button is a type of View, so we can pass that in (similar to either
-    //polymorphism or inheritance idk the difference LOL)) 
+    //polymorphism or inheritance idk the difference LOL)) - Jeraldin
     @Override
     public void onClick(View v) {
         //makes a new Intent so that you're able to set it up to go to the next place
         //intent( where you are, to where you want to go)
         Intent nextActivity = new Intent(AboutMarathon.this, MapsActivity.class);
+
+        //puts in an extra string so in the
         nextActivity.putExtra("next", "AboutMarathon");
 
         //actually starts that intent 
--- PahktheCah/app/src/main/java/com/example/jeraldinguerrero/pahkthecah/MapsActivity.java
+++ PahktheCah/app/src/main/java/com/example/jeraldinguerrero/pahkthecah/MapsActivity.java
@@ -56,9 +56,6 @@ public class MapsActivity extends FragmentActivity implements
         //Set up map click listeners
         mMap.setOnMapClickListener(this);
         mMap.setOnMapLongClickListener(this);
-        // Zoom in the Google Map
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(42.3496, -71.0997), 14.0f));
-        // mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(39.8282, -98.5795), 3.0f));
     }
     //Set up the map the first time
     private void setUpMapIfNeeded()
@@ -76,9 +73,7 @@ public class MapsActivity extends FragmentActivity implements
         }
     }
 
-
-
-    //Passes in Map to do some intialization nothing in this case
+    //Passes in Map
     public void onMapReady(GoogleMap googleMap)
     {
         Intent nextActivity = getIntent();
@@ -561,56 +556,3 @@ public class MapsActivity extends FragmentActivity implements
     }
 
 }
-
-/*
-import android.content.Intent;
-import android.net.Uri;
-import android.support.v4.app.FragmentActivity;
-import android.os.Bundle;
-
-import com.google.android.gms.maps.CameraUpdateFactory;
-import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
-
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
-
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_maps);
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
-    }
-
-
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-
-
-    public void onMapReady(GoogleMap googleMap) {
-
-       Intent nextActivity = getIntent();
-        String next = nextActivity.getStringExtra("next");
-
-
-
-        else
-            map.addMarker(new MarkerOptions()
-                    .position(new LatLng(42.349220, -71.106121))
-                    .title("Photonics Center"));
-    }
-}
-*/
